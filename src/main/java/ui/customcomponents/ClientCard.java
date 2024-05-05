4651705ce627bab2e329f6177a69901145158d44
ismail <ismailerramdany@gmail.com>
2024-05-05 15:54:35 +0100

    Update ClientCrad class to display client information fetched from database and pass down client ID to ListAppartement

--- src/main/java/ui/customcomponents/ClientCard.java
+++ src/main/java/ui/customcomponents/ClientCard.java
@@ -1,25 +1,33 @@
 package ui.customcomponents;
 
+import com.nezukoRent.database.UserData;
+import com.nezukoRent.managment.ListAppartements;
+import com.nezukoRent.managment.Login;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dialog;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
+import java.awt.Font;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ClientCard extends JPanel {
+public class ClientCard extends RoundJPanel {
+    private Login loginFrame;
     private JLabel userIcon;
     private JLabel fullNameLabel;
     private JLabel dateOfJoinLabel;
     private JLabel numberOfRentsLabel;
     private JPanel informationPanel;
     
-    public ClientCard() {
-        String fullName = "ISMAIL ER-RAMDANY";
+    public ClientCard(int radius,Login loginFrame,UserData userInfo) {
+        super(radius);
+        this.loginFrame = loginFrame;
+        String fullName = userInfo.getFirstName() + " " + userInfo.getLastName();
         String dateOfJoin = "2023/12/12";
         int numberOfRents = 10;
         
@@ -28,10 +36,13 @@ public class ClientCard extends JPanel {
         
         JPanel container = new JPanel(new BorderLayout());
         
+        Font textLarge = new Font("Liberation Sans",1,14);
+        Font textSmall = new Font("Liberation Sans",1,12);
+        
         this.userIcon = new JLabel("");
-        this.userIcon.setIcon(new ImageIcon("C:\\Users\\INFODAK\\Documents\\NetBeansProjects\\J2eeProject\\src\\main\\resources\\icons8-user-50.png"));
+        this.userIcon.setIcon(scaleImageIcon(new ImageIcon(getClass().getResource("/icons/user.png")),70,50));
         userIcon.setHorizontalAlignment(javax.swing.SwingConstants.LEADING);
-        userIcon.setPreferredSize(new Dimension(70,50));
+        userIcon.setPreferredSize(new Dimension(90,100));
         
         this.informationPanel = new JPanel(new BorderLayout());
         
@@ -39,21 +50,28 @@ public class ClientCard extends JPanel {
         JPanel panel2 = new JPanel(new BorderLayout());
         
         this.fullNameLabel = new JLabel(fullName);
-        this.fullNameLabel.setFont(new java.awt.Font("Segoe UI", 1, 14));
-        JPanel panel11 = new JPanel(new FlowLayout());
+        this.fullNameLabel.setFont(textLarge);
+        JPanel panel11 = new JPanel(new FlowLayout(FlowLayout.RIGHT,15,0));
         
-        JLabel editIcon = new JLabel(scaleImageIcon(new ImageIcon("C:\\Users\\INFODAK\\Documents\\NetBeansProjects\\J2eeProject\\src\\main\\resources\\icons8-edit-50.png"),30,30));
-        JLabel plusIcon = new JLabel(scaleImageIcon(new ImageIcon("C:\\Users\\INFODAK\\Documents\\NetBeansProjects\\J2eeProject\\src\\main\\resources\\icons8-plus-50.png"),30,30));
-        JLabel deleteIcon = new JLabel(scaleImageIcon(new ImageIcon("C:\\Users\\INFODAK\\Documents\\NetBeansProjects\\J2eeProject\\src\\main\\resources\\icons8-trash-64.png"),30,30));
+        JLabel editIcon = new JLabel(scaleImageIcon(new ImageIcon(getClass().getResource("/icons/edit.png")),30,30));
+        JLabel plusIcon = new JLabel(scaleImageIcon(new ImageIcon(getClass().getResource("/icons/add_32inactive32.png")),30,30));
+        JLabel deleteIcon = new JLabel(scaleImageIcon(new ImageIcon(getClass().getResource("/icons/wast_basket.png")),28,28));
         
         editIcon.setSize(new Dimension(30,30));
         plusIcon.setSize(new Dimension(30,30));
-        deleteIcon.setSize(new Dimension(30,30));
+        deleteIcon.setSize(new Dimension(28,28));
         
         editIcon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         plusIcon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         deleteIcon.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
 
+        // add event Handler to Plus ICON
+        plusIcon.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                assignAppartementMouseClicked(evt,userInfo.getId());
+            }
+        });
         
         panel11.add(editIcon);
         panel11.add(plusIcon);
@@ -66,9 +84,9 @@ public class ClientCard extends JPanel {
         
         
         this.dateOfJoinLabel = new JLabel("member since : " + dateOfJoin);
-        this.dateOfJoinLabel.setFont(new java.awt.Font("Segoe UI", 1, 12));
+        this.dateOfJoinLabel.setFont(textSmall);
         this.numberOfRentsLabel = new JLabel("active rents : " + numberOfRents);
-        this.numberOfRentsLabel.setFont(new java.awt.Font("Segoe UI", 1, 12));
+        this.numberOfRentsLabel.setFont(textSmall);
         
         panel2.add(numberOfRentsLabel,BorderLayout.WEST);
         panel2.add(dateOfJoinLabel,BorderLayout.EAST);
@@ -90,20 +108,18 @@ public class ClientCard extends JPanel {
         this.setMaximumSize(new Dimension(450,95));
         this.setBorder(javax.swing.BorderFactory.createEmptyBorder());
     }
-    
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        int width = getWidth();
-        int height = getHeight();
-        int arcSize = 20; 
-        g.setColor(getBackground());
-        g.fillRoundRect(0, 0, width, height, arcSize, arcSize);
-        g.setColor(getForeground());
-        g.drawRoundRect(0, 0, width - 1, height - 1, arcSize, arcSize);
-    }
+    private void assignAppartementMouseClicked(java.awt.event.MouseEvent evt,int clientId) {                                      
+        ListAppartements desAppartements = new ListAppartements(this.loginFrame,clientId);
+        JDialog overlayDialog = new JDialog(this.loginFrame, "Assigne appartement", Dialog.ModalityType.APPLICATION_MODAL);
+        overlayDialog.setContentPane(desAppartements);
+        overlayDialog.setSize(950, 623);
+        overlayDialog.setLocationRelativeTo(this.loginFrame);
+        overlayDialog.setVisible(true);
+
+    }                                     
     private static ImageIcon scaleImageIcon(ImageIcon icon, int width, int height) {
         Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
         return new ImageIcon(image);
     }
 }
+
