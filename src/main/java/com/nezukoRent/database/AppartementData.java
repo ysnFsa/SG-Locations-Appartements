d4814c8f53af623e0c011e58de31b7832c3708aa
yassine chouikh <yassine.chouikh.fsa@gmail.com>
2024-05-09 15:38:14 +0100

    Implemented XML import/export functionality for apartment

--- src/main/java/com/nezukoRent/database/AppartementData.java
+++ src/main/java/com/nezukoRent/database/AppartementData.java
@@ -8,6 +8,13 @@ package com.nezukoRent.database;
  *
  * @author yassin
  */
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "appartement")
+
 public class AppartementData {
     private int id;
     private String type;
@@ -18,8 +25,12 @@ public class AppartementData {
     private int ville_id;
     private int quartier_id;
     private String descreption;
-    private int contratID;
+      private int contratID;
+          private List<PhotosData> photosDataList;
 
+ public AppartementData() {
+        // Initialize default values if needed
+    }
     public AppartementData(int id, String type, double surface, int chambres, boolean disponibilite, double prix, int ville_id, int quartier_id, String descreption) {
         this.id = id;
         this.type = type;
@@ -31,7 +42,7 @@ public class AppartementData {
         this.quartier_id = quartier_id;
         this.descreption=descreption;
     }
-    public AppartementData(int id, String type, double surface, int chambres, boolean disponibilite, double prix, int ville_id, int quartier_id, String descreption,int contratID) {
+        public AppartementData(int id, String type, double surface, int chambres, boolean disponibilite, double prix, int ville_id, int quartier_id, String descreption,int contratID) {
         this.id = id;
         this.type = type;
         this.surface = surface;
@@ -79,7 +90,36 @@ public class AppartementData {
     public String getDescreption(){
         return this.descreption;
     }
-    public int getContratId() {
+     public int getContratId() {
         return this.contratID;
     }
+    public String getDescription() {
+    return this.descreption;
+}
+    
+       @XmlElement(name = "photos")
+    public List<PhotosData> getPhotosDataList() {
+        return photosDataList;
+    }
+
+    public void setPhotosDataList(List<PhotosData> photosDataList) {
+        this.photosDataList = photosDataList;
+    }
+    
+    @Override
+public String toString() {
+    return "AppartementData{" +
+            "id=" + id +
+            ", type='" + type + '\'' +
+            ", surface=" + surface +
+            ", chambres=" + chambres +
+            ", disponibilite=" + disponibilite +
+            ", prix=" + prix +
+            ", ville_id=" + ville_id +
+            ", quartier_id=" + quartier_id +
+            ", descreption='" + descreption + '\'' +
+            '}';
+}
+    
+    
 }
